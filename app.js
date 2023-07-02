fc9e8727de5b5a8dab1f40c10d5b98213ae87965
ArielCesari <ariel.dcesari@gmail.com>
2023-07-02 12:42:41 -0300

    Creo vista de registro de usuario

--- app.js
+++ app.js
@@ -9,5 +9,9 @@ app.get('/',(req,res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 } )
 
+app.get('/register',(req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+} )
+
 
 app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'))
